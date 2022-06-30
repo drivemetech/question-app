1e908ff914dc450244c0483f20a3331a199d1a60
Adamu Abdulkarim <drivemetech@gmail.com>
2022-06-29 22:21:13 -0700

    added user profile

---
+++ myaudioapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.login, name='login'),
+    path('index/', views.index, name='index'),
+    path('questions/<slug:slug>/', views.viewQuestion, name='view-Question'),
+    path('question/<int:pk>/answer/', views.My_Answer.as_view(), name='answer'),
+    path('question/', views.My_Question.as_view(), name='question'),
+    path('register/', views.register, name='register'),
+    path('notification/', views.NotificationListView.as_view(), name='notification'),
+    path('answer/<int:pk>/like', views.like_answer, name='like-answer'),
+    path('userProfile/<slug:slug>/', views.public_profile, name='Public_Profile'),
+]
