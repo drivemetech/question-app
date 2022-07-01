9bba7497e7c7cf672d75b9ee083f2d371aa99ed1
ErikKalkoken <erik.kalkoken@gmail.com>
2022-07-01 13:11:11 +0200

    Show profiles as inline to users on admin site

--- myaudioapp/admin.py
+++ myaudioapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
 from .models import Answer, Question, Notification, Category, Like, Profile
 
 # Register your models here.
@@ -8,4 +11,18 @@ admin.site.register(Question)
 admin.site.register(Notification)
 admin.site.register(Category)
 admin.site.register(Like)
-admin.site.register(Profile)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
