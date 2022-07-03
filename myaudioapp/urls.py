b931ee9b5f33358d8e4e726182ab7259abf0236f
Drive Me Tech <97406244+drivemetech@users.noreply.github.com>
2022-07-03 15:45:20 -0700

    added forgot password

--- myaudioapp/urls.py
+++ myaudioapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.login, name='login'),
@@ -11,4 +12,30 @@ urlpatterns = [
     path('notification/', views.NotificationListView.as_view(), name='notification'),
     path('answer/<int:pk>/like', views.like_answer, name='like-answer'),
     path('userProfile/<slug:slug>/', views.public_profile, name='Public_Profile'),
+    
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+            template_name='registration/password_reset_form.html',
+            success_url='password_reset_confirm'
+         ),
+         name='password_reset'),
+
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='registration/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='registration/password_reset_confirm.html',
+             success_url=' ' 
+         ),
+         name='password_reset_confirm'),
+
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='registration/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
 ]
